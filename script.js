e04d256378fc0418d0db9329c35afc075122450a
tommybautista <100245701+tommybautista@users.noreply.github.com>
2022-04-07 19:12:59 -0400

    added files

---
+++ script.js
@@ -0,0 +1,56 @@
+function bigImg0(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+    x.src="assets/tristan0updated.jpg";
+  }
+  
+  function normalImg0(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+    x.src="assets/tristan0.jpg";
+  }
+  
+  function bigImg1(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+    x.src="assets/tristan1updated.jpg";
+  }
+  
+  function normalImg1(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+    x.src="assets/tristan1.jpg";
+  }
+  
+  function bigImg2(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+    x.src="assets/tristan2updated.jpg";
+  }
+  function normalImg2(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+    x.src="assets/tristan2.jpg";
+  }
+  
+  function bigImg3(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+    x.src="assets/tristan3updated.jpg";
+  }
+  function normalImg3(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+    x.src="assets/tristan3.jpg";
+  }
+  
+  function bigImg4(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+    x.src="assets/tristan4updated.jpg";
+  }
+  function normalImg4(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+    x.src="assets/tristan4.jpg";
+  }
